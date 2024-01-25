a8c7f7f1b315c18846e9110b38fae870795a3235
Felix Ouma <moxiemeyoh@gmail.com>
2024-01-25 09:30:54 +0300

    Add: Signup UI - laying out the UI - getting the user input data - loging the signup details to console

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Home, Members, Contacts, Activities, About  } from "./pages";
 import Footer from "./components/Footer";
 import BecomeAmember from "./components/BecomeAmember";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/contact-us" element={<Contacts/>}/>
           <Route path="/become-amember" element={<BecomeAmember/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
         <Footer/>
     </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,92 @@
-
+import { useState } from "react";
+import { BiShow, BiHide } from "react-icons/bi";
 
 const Signup = () => {
+    const[showPassword, setShowPassword] = useState(false);
+    const [signupDetails, setSignupDetails] = useState({
+        fullName: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+    });
+
+    // collect data from signup form
+    const handleOnchange = (e) =>{
+        const {name, value} = e.target;
+        setSignupDetails((prev) => {
+            return {
+                ...prev,
+                [name] :value
+            }
+        })
+    }
+
+    // submition
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const {email, password} = signupDetails;
+        if (email && password) {
+            console.log(signupDetails);
+            // post the information to the backend from here
+        }
+    }
+
     return (
-        <div>Signup</div>
+        <div className="min-h-[100vh] grid content-center">
+            <div className="mx-auto max-w-md w-full shadow-md px-2 py-3">
+                <form className="w-full flex flex-col gap-2" onSubmit={handleSubmit}>
+                    <label htmlFor="fullName">full Name:</label>
+                    <input 
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    value={signupDetails.fullName}
+                    onChange={handleOnchange}
+                    className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+                    />
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={signupDetails.email}
+                    onChange={handleOnchange}
+                    className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+                    />
+                    <label htmlFor="password">Password:</label>
+                    <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+                        <input
+                        className=" w-full bg-slate-200 border-none outline-none" 
+                        type= {showPassword? "text": "password"} 
+                        name="password"
+                        id="password"
+                        value={signupDetails.password}
+                        onChange={handleOnchange}
+                        />
+                        <span 
+                        className="flex text-xl cursor-pointer"
+                        onClick={()=>setShowPassword((prev) => !prev)}
+                        >
+                            { showPassword ? <BiShow/> : <BiHide/> }
+                        </span>
+                    </div>
+                    <label htmlFor="passwordConfirm">Confirm Password:</label>
+                    <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+                        <input
+                        className=" w-full bg-slate-200 border-none outline-none" 
+                        type= {showPassword? "text": "password"} 
+                        name="passwordConfirm"
+                        id="passwordConfirm"
+                        value={signupDetails.passwordConfirm}
+                        onChange={handleOnchange}
+                        />
+                    </div>
+                    <button>
+                        submit
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 
